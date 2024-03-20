3ad083b5cb7d7c4f0244f6968e95bd5218fa3d91
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2024-03-20 22:00:02 +0530

    fix: can match auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'admin/login/:id', component: LoginComponent },
-  // { path: 'v1', loadChildren:  },
   {
     path: 'admin',
     loadChildren: () =>
       import('@sc-modules/admin/admin.module').then((m) => m.AdminModule),
+    canMatch: [authGuard],
   },
   { path: '**', redirectTo: '/' },
 ];
