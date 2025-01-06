6a50037c342a44024c05788c27e9b3c51efb5ec2
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2025-01-07 00:08:24 +0530

    fixed issues with date adapter and radio validation

--- src/app/core/modules/forms/date-element/date-element.component.ts
+++ src/app/core/modules/forms/date-element/date-element.component.ts
@@ -1,16 +1,38 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 import { DateInput } from '@sc-models/form';
 import { ScreenSizeObserver } from 'src/app/core/service/screen.service';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import * as _moment from 'moment';
+import { default as _rollupMoment } from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'sc-date-element',
   templateUrl: './date-element.component.html',
   styleUrl: './date-element.component.scss',
   standalone: false,
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ]
 })
-export class DateElementComponent {
+export class DateElementComponent implements OnChanges {
   screenObserver = inject(ScreenSizeObserver);
 
   readonly element = input.required<DateInput>();
@@ -34,4 +56,17 @@ export class DateElementComponent {
         .map(([className]) => className);
     return classes.join(' ');
   };
+  max: Date | null = null
+  min: Date | null = null
+  ngOnChanges() {
+    const element = this.element();
+    this.max = element.max? new Date(element.max) : null;
+    this.min = element.min? new Date(element.min) : null;
+    if (element.disabled) {
+      this.control().disable();
+    } else {
+      this.control().enable();
+    }
+    this.control().setValue(element.value);
+  }
 }
