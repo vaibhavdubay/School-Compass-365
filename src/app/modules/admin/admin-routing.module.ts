6d4f968069c25bb4cd1018964a0430ad82582650
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2025-02-02 12:02:30 +0530

    fix: correct spelling of 'preference' in routing and layout components

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
         },
       },
       {
-        path: 'preferance',
+        path: 'preference',
         component: EditProfileComponent,
         data: {
           title: 'Preference',
